6ae9696cc5dd1c494fddb0bd56a6aed9fc761315
Vabalokis <vabalokis@yahoo.com>
2018-07-01 12:59:22 +0300

    style formatting

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,43 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService, IMessage } from './contact.service';
-import { NgModel } from '@angular/forms';
-import { AgmCoreModule } from '@agm/core';
+import { Component, OnInit } from "@angular/core";
+import { AppService, IMessage } from "./contact.service";
+import { NgModel } from "@angular/forms";
+import { AgmCoreModule } from "@agm/core";
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
-
-
   lat = 54.910705;
   lng = 23.919183;
   zoom = 14;
 
   message: IMessage = {};
 
+  constructor(private appService: AppService) {}
 
-  constructor(private appService: AppService) {
-
-  }
-
-
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 
   sendEmail(message: IMessage) {
-
-    this.appService.sendEmail(message).subscribe(res => {
-      console.log('AppComponent Success', res);
-    }, error => {
-      console.log('AppComponent Error', error);
-    });
+    this.appService.sendEmail(message).subscribe(
+      res => {
+        console.log("AppComponent Success", res);
+      },
+      error => {
+        console.log("AppComponent Error", error);
+      }
+    );
   }
-
-
-
-
-
 }
--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { Resolve } from '@angular/router';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import { Resolve } from "@angular/router";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 export interface IMessage {
   name?: string;
@@ -13,55 +13,58 @@ export interface IMessage {
 
 @Injectable()
 export class AppService {
-
-  private emailUrl = '/assets/email.php';
+  private emailUrl = "/assets/email.php";
   private switch = 0; // Display message state switch 0 - nothing is showing , 1 - message is now displayed
 
-  constructor(private http: Http) {
-
-  }
+  constructor(private http: Http) {}
 
   messageboxFlash(msgstring) {
     if (this.switch === 0) {
       this.switch = 1;
-      const statbx = document.getElementById('statusbox');
-      statbx.style.display = 'block';
-      statbx.style.opacity = '1';
+      const statbx = document.getElementById("statusbox");
+      statbx.style.display = "block";
+      statbx.style.opacity = "1";
       statbx.innerHTML = msgstring;
-      setTimeout(x => {statbx.innerHTML = ''; statbx.style.display = 'none'; this.switch = 0; }, 4000);
+      setTimeout(x => {
+        statbx.innerHTML = "";
+        statbx.style.display = "none";
+        this.switch = 0;
+      }, 4000);
     }
   }
 
   clearForm() {
-    (<HTMLInputElement>document.getElementById('nametextbox')).value = '';
-    (<HTMLInputElement>document.getElementById('emailtextbox')).value = '';
-    (<HTMLInputElement>document.getElementById('messagetextbox')).value = '';
+    (<HTMLInputElement>document.getElementById("nametextbox")).value = "";
+    (<HTMLInputElement>document.getElementById("emailtextbox")).value = "";
+    (<HTMLInputElement>document.getElementById("messagetextbox")).value = "";
   }
 
   sendEmail(message: IMessage): Observable<IMessage> | any {
-    const nameval =  (<HTMLInputElement>document.getElementById('nametextbox')).value ;
-    const emailval =  (<HTMLInputElement>document.getElementById('emailtextbox')).value ;
-    const messageval =  (<HTMLInputElement>document.getElementById('messagetextbox')).value ;
+    const nameval = (<HTMLInputElement>document.getElementById("nametextbox"))
+      .value;
+    const emailval = (<HTMLInputElement>document.getElementById("emailtextbox"))
+      .value;
+    const messageval = (<HTMLInputElement>(
+      document.getElementById("messagetextbox")
+    )).value;
 
-    if (nameval !== '' && emailval !== '' && messageval !== '') {
+    if (nameval !== "" && emailval !== "" && messageval !== "") {
       this.clearForm();
-      return this.http.post(this.emailUrl, message)
-      .map(response => {
-        console.log('Sending email was successfull', response);
-        this.messageboxFlash('Message has been successfully sent');
-        return response;
-      })
-      .catch(error => {
-        console.log('Sending email got error', error);
-        this.messageboxFlash('There has been an error sending a message');
-        return Observable.throw(error);
-      });
-
+      return this.http
+        .post(this.emailUrl, message)
+        .map(response => {
+          console.log("Sending email was successfull", response);
+          this.messageboxFlash("Message has been successfully sent");
+          return response;
+        })
+        .catch(error => {
+          console.log("Sending email got error", error);
+          this.messageboxFlash("There has been an error sending a message");
+          return Observable.throw(error);
+        });
     } else {
-      this.messageboxFlash('Fill all text fields');
-      return this.http.post('', null); // ???
+      this.messageboxFlash("Fill all text fields");
+      return this.http.post("", null); // ???
     }
-
-
   }
 }
